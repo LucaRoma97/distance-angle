2b8b2acca71045aa31f86ee3143f056a31fe56de
Luca <romanelloluca97@libero.it>
2020-07-14 12:41:25 -0700

    adjustments on cmake

--- src/distance_angle.cpp
+++ src/distance_angle.cpp
@@ -1,20 +1,15 @@
-#include <ar_track_alvar_msgs/AlvarMarkers.h>
-#include "ros/ros.h"
-#include "distance_angle/DistanceAngle.h"
+#include <ros/ros.h>
 #include "std_msgs/Float64.h"
-#include <math.h> 
-#define PI 3.14159265
-#define baseline 0.12
+#include "tf2_msgs/TFMessage.h"
+#include "distanceangle/DistanceAngle.h"
 #define flags 1
+#define PI 3.14
 
+
+ros::Publisher OdomAngle_pub;
 ros::Publisher DistanceAngle_pub;
-distance_angle::DistanceAngle marker;
-
-void odomangleCallback(const std_msgs::Float64& odom)
-{
-   odomangle = odom.data;
-}
-
+distanceangle::DistanceAngle marker;
+//float chassis {0.05};
 
 class MarkerParameters
 {
@@ -22,96 +17,145 @@ class MarkerParameters
 public:
     float distance;
     float angle;
+    float Yaw_odom;
+    float Pitch_odom;
+    float Roll_odom;
     float Yaw;
     float Pitch;
     float Roll;
     
-    void Computedistangle(const ar_track_alvar_msgs::AlvarMarkers& ar_marker, int i) {
-        
-        float x,y,xp,yp;
-        
-        xp = ar_marker.markers[i].pose.pose.position.x;
-        
-        yp = ar_marker.markers[i].pose.pose.position.y;
+    void ComputeOdomAngle(const tf2_msgs::TFMessage& robot, int i){
 
-        x = cos(odomangle)*xp + sin(odomangle)*yp;
+	float q0,q1,q2,q3;
+   
+    	q0 = robot.transforms[0].transform.rotation.x;
 
-	y = -sin(odomangle)*xp + cos(odomangle)*yp;
+    	q1 = robot.transforms[0].transform.rotation.y;
+
+        q2 = robot.transforms[0].transform.rotation.z;
+
+        q3 = robot.transforms[0].transform.rotation.w;
+    
+  	Yaw_odom = atan2(2*(q0*q1 + q2*q3),1-2*(q1*q1+q2*q2))*180/PI;
+
+        Pitch_odom = asin(2*(q0*q2-q3*q1))*180/PI;
+             
+        Roll_odom = atan2(2*(q0*q3 + q2*q1),1-2*(q0*q0+q1*q1))*180/PI;
+
+}
+
+    void Computedistangle(const tf2_msgs::TFMessage& ar_marker, int i) {
+        
+        float x,y,xp,yp,Yaw_odom_rad;
+
+	Yaw_odom_rad = Yaw_odom*PI/180;
+        
+        xp = ar_marker.transforms[i].transform.translation.z;
+        
+        yp = ar_marker.transforms[i].transform.translation.x;
+
+        x = cos(Yaw_odom_rad)*xp + sin(Yaw_odom_rad)*yp; //+ chassis*(1-cos(Yaw_odom_rad));
+
+	y = -sin(Yaw_odom_rad)*xp + cos(Yaw_odom_rad)*yp; // - chassis*sin(Yaw_odom_rad); //check minus
+
+	//ROS_INFO("x: [%f], y: [%f], Y: [%f]", x, y, sin(Yaw_odom_rad));
 
         distance = sqrt(x*x + y*y);
 
-        angle = atan(y/x)*180/PI;
+        angle = atan(y/x)*180/PI; 
         
     }
     
-    void ComputeRPY(const ar_track_alvar_msgs::AlvarMarkers& ar_marker, int i) {  /* from quaternion to euler angles */
+    void ComputeRPY(const tf2_msgs::TFMessage& ar_marker, int i) {  /* from quaternion to euler angles */
             
-            float q0,q1,q2,q3;
+        float q0,q1,q2,q3;
             
-            q0 = ar_marker.markers[i].pose.pose.orientation.x;
+        q0 = ar_marker.transforms[i].transform.rotation.x;
         
-            q1 = ar_marker.markers[i].pose.pose.orientation.y;
+        q1 = ar_marker.transforms[i].transform.rotation.y;
 
-            q2 = ar_marker.markers[i].pose.pose.orientation.z;
+        q2 = ar_marker.transforms[i].transform.rotation.z;
 
-            q3 = ar_marker.markers[i].pose.pose.orientation.w;
+        q3 = ar_marker.transforms[i].transform.rotation.w;
             
-            Yaw = atan2(2*(q0*q1 + q2*q3),1-2*(q1*q1+q2*q2))*180/PI;
+        Yaw = atan2(2*(q0*q1 + q2*q3),1-2*(q1*q1+q2*q2))*180/PI;
 
-            Pitch = asin(2*(q0*q2-q3*q1))*180/PI;
+        Pitch = asin(2*(q0*q2-q3*q1))*180/PI;
             
-            Roll = atan2(2*(q0*q3 + q2*q1),1-2*(q0*q0+q1*q1))*180/PI;
+        Roll = atan2(2*(q0*q3 + q2*q1),1-2*(q0*q0+q1*q1))*180/PI;
             
     }
 
-     void CopytoMarker(distance_angle::DistanceAngle &marker){
+     void CopytoMarker(distanceangle::DistanceAngle &marker){
 
 	    marker.distance = distance;
 	    marker.angle = angle;
-	    marker.orientation = Yaw + 90 + odomangle; //for having 0 degree into the perpendicular line
+	    marker.orientation = Yaw + Yaw_odom; //for having 0 degree into the perpendicular line
     }
 
 };
 
+MarkerParameters ARmarker;
 
+void odomangleCallback(const tf2_msgs::TFMessage robot)
 
-void distanceangleCallback(const ar_track_alvar_msgs::AlvarMarkers ar_pose_marker)
 {
-  
-  int i;
+
+     std_msgs::Float64 odomangle;
+
+     int i;
 
   for (i=0;i<flags;++i)
     {
 
-        MarkerParameters ARmarker;
+
+  if ((robot.transforms[0].header.frame_id == "odom")&&(robot.transforms[0].child_frame_id == "chassis"))
+
+  {
         
-        ARmarker.Computedistangle(ar_pose_marker,i);
+        ARmarker.ComputeOdomAngle(robot,i);
+
+  }
+
+
+  if ((robot.transforms[0].header.frame_id == "camera_link_optical")&&(robot.transforms[0].child_frame_id == "ar_marker_2")) //I should define the frames above!
+ {
+
+        ARmarker.Computedistangle(robot,i);
         
-        ARmarker.ComputeRPY(ar_pose_marker,i); 
+        ARmarker.ComputeRPY(robot,i); 
   
-	ARmarker.CopytoMarker(marker);
+	ARmarker.CopytoMarker(marker);   
+ }
 
-        ROS_INFO("Distance: [%f], angle: [%f], orientation: [%f]", odomangle, marker.angle, marker.orientation);
+    ROS_INFO("Distance: [%f], angle: [%f], orientation: [%f]", marker.distance, marker.angle, marker.orientation);
 
-        DistanceAngle_pub.publish(marker);
+    DistanceAngle_pub.publish(marker);
+    
+    odomangle.data = ARmarker.Yaw_odom;
 
-    }
-}
+    OdomAngle_pub.publish(odomangle);
+  
+  }
+
+ }
+
+
 
 int main(int argc, char **argv)
 { 
  
   ros::init(argc, argv, "distance_angle");
 
-  ros::NodeHandle n, nh;
+  ros::NodeHandle n;
 
-  DistanceAngle_pub = n.advertise<distance_angle::DistanceAngle>("DistanceAngle", 1000);
+  ros::Subscriber sub = n.subscribe("/tf", 1,odomangleCallback);
 
- ros::Subscriber odom_sub = nh.subscribe("/odomangle", 1, odomangleCallback);
-
-  ros::Subscriber sub = n.subscribe("/ar_pose_marker", 1, distanceangleCallback);
+  OdomAngle_pub = n.advertise<std_msgs::Float64>("odomangle", 1000);
+ 
+  DistanceAngle_pub = n.advertise<distanceangle::DistanceAngle>("DistanceAngle", 1000);
 
   ros::spin();
-
+  
   return 0;
 }
